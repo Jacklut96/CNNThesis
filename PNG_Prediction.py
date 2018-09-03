de734e9a3fa1fc4701f2e63e82a8eeb9d445dd3a
Jacklut96 <41819591+Jacklut96@users.noreply.github.com>
2018-09-03 18:04:57 +0200

    Versione Figa per Dwarf

--- PNG_Prediction.py
+++ PNG_Prediction.py
@@ -1,15 +1,19 @@
 import pandas as pd
 import csv
-import os 
-import sys
+import os
+import re
 import keras
 from keras.models import load_model
 from keras.preprocessing import image
 from keras.utils import np_utils
 import numpy as np 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
-path ="/Users/utente/Desktop/Test"
+path ="/home/users/emanuele.tauro/Desktop/totale/"
+n_samples = 0 
+good =  0
+fn = 0 
+fp = 0
 
 #LABEL READING (Future work ------> FILENAME CHECK)
 #with open ('/Users/utente/Desktop/ND_CSV/labels.csv') as labelfile:
@@ -30,30 +34,59 @@ path ="/Users/utente/Desktop/Test"
 
 
 #-------MODEL LOADING-----------------
-model = load_model('/Users/utente/Desktop/Progetto/CODE/Inception/Inception.h5')
+model = load_model('/home/users/emanuele.tauro/Desktop/models/Inceptionfullr42.h5')
 img_width = 140
 img_height = 140 
 
-#-------PREDICTION--------------------
+#-------PREDICTION AND RESULT--------------------
 for filename in os.listdir(path):
     if filename.endswith(".png"):
+        n_samples += 1
         img = image.load_img(os.path.join(path,filename), target_size=(img_width, img_height)) 
 
         # convert image to numpy array, so Keras can render a prediction
         img_arr = image.img_to_array(img)
-        img_arr = (np.maximum(img_arr,0) / img_arr.max())
+        #img_arr = (np.maximum(img_arr,0) / img_arr.max())
 
         # expand array from 3 dimensions (height, width, channels) to 4 dimensions (batch size, height, width, channels)
         # rescale pixel values to 0-1
-        x = np.expand_dims(img, axis=0) * 1./255
+        x = np.expand_dims(img_arr, axis=0) * 1./255
 
-        # get prediction on test image
+        # get prediction on test image 
         score = model.predict(x)
         text = ("%s" %score + ": lung" if score < 0.5 else "%s" %score + ": not lung")
-        plt.imshow(img)
-        plt.text(0,(-2.5),text)
-        plt.show()
+        #append predictions to have quantitative results
+    
+        prediction = 1 if score <0.5 else 0
+        
+        #if (score<=0.5):
+        #    prediction = 1
+        #if (score>0.5):
+        #    prediction = 0
 
+        if re.match("^lung", filename):
+            truth = 1
+        if re.match("^not", filename):
+            truth = 0
+
+        if (prediction==truth):
+            good += 1
+
+        if(prediction==1 and truth==0):
+            fp += 1
+        if(prediction==0 and truth==1):
+            fn += 1
+
+        #==Plotting (can be commented safely)====
+        #plt.imshow(img)
+        #plt.text(0,(-2.5),text)
+        #plt.show()
+
+
+accuracy = (good/n_samples)*100
+print("Accuracy: %f" %accuracy)
+print("False positives: %i" %fp)
+print("False negatives: %i" %fn)
 
 #----------PREDICTION RESULT---------------- (TODO: implement label check)
 #result = model.predict(pixel_array)
